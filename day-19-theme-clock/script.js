922816bbc8f727359233fddabaf2d21513e1cc6e
Bianca Nazareno <404heracles@gmail.com>
2022-10-21 17:09:42 +0800

    Theme Clock v2 - improved toggle and color

--- day-19-theme-clock/script.js
+++ day-19-theme-clock/script.js
@@ -1,10 +1,16 @@
 // Variables
+const body = document.querySelector("body");
+const toggle = document.querySelector("#toggle");
+const sunIcon = document.querySelector(".toggle .bxs-sun");
+const moonIcon = document.querySelector(".toggle .bx-moon");
+
+// const body = document.querySelector("body");
+// const toggle = document.querySelector('.toggle')
 const hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
-const toggle = document.querySelector('.toggle')
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
@@ -14,17 +20,32 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
     return(num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-// Theme Toggle
-toggle.addEventListener('click', (e) => {
-    const html = document.querySelector('html')
-    if(html.classList.contains('dark')){
-        html.classList.remove('dark')
-        e.target.innerHTML = 'Dark Mode'
-    }else {
-        html.classList.add('dark')
-        e.target.innerHTML = 'Light Mode'
-    }
-})
+
+
+
+// Old Theme Toggle
+// toggle.addEventListener('click', (e) => {
+//     const html = document.querySelector('html')
+//     if(html.classList.contains('dark')){
+//         html.classList.remove('dark')
+//         // e.target.innerHTML = 'Dark Mode'
+//     }else {
+//         html.classList.add('dark')
+//         // e.target.innerHTML = 'Light Mode'
+//     }
+// })
+
+//New Theme Toggle
+toggle.addEventListener("change", () => {
+
+    body.classList.toggle("dark");
+    sunIcon.className = sunIcon.className === "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+    moonIcon.className = moonIcon.className === "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+
+});
+
+
+
 
 function setTime(){
     const time = new Date()
